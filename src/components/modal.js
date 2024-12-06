0beab9a7cae42b6083dfdf4cc1356bff2a5749aa
Mikhail Burykin <whitecrazygoose@mail.ru>
2024-12-06 15:53:25 +0300

    modal refactoring and checks for undefined/null

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,20 +1,34 @@
-export { openPopup, closePopup }
+export { openPopup, closePopup };
 
-//   Закрытие по кнопке ESC
+/**
+ * Закрытие попапа при нажатии на клавишу Escape.
+ * @param {KeyboardEvent} evt - Объект события клавиатуры.
+ */
 function closeOnEsc(evt) {
   if (evt.key === 'Escape') {
-    const currentPopup = document.querySelector('.popup_opened')
-    closePopup(currentPopup)
+    const currentPopup = document.querySelector('.popup_opened'); // Находим текущий открытый попап
+    closePopup(currentPopup);
   }
 }
 
-//   Удаляем слушатель на закрытие попапов по кнопке ESC
+/**
+ * Закрытие попапа.
+ * Убирает класс открытого попапа и удаляет обработчик события нажатия клавиши Escape.
+ * @param {HTMLElement} popupElement - Элемент попапа, который нужно закрыть.
+ */
 function closePopup(popupElement) {
-  popupElement.classList.remove('popup_opened');
-  document.removeEventListener('keydown', closeOnEsc)
+  if (!popupElement) return; // Проверяем, передан ли элемент
+  popupElement.classList.remove('popup_opened'); // Убираем класс, открывающий попап
+  document.removeEventListener('keydown', closeOnEsc); // Удаляем слушатель закрытия по ESC
 }
 
+/**
+ * Открытие попапа.
+ * Добавляет класс для отображения попапа и навешивает обработчик закрытия по клавише Escape.
+ * @param {HTMLElement} popupElement - Элемент попапа, который нужно открыть.
+ */
 function openPopup(popupElement) {
-  popupElement.classList.add('popup_opened');
-  document.addEventListener('keydown', closeOnEsc);
+  if (!popupElement) return; // Проверяем, передан ли элемент
+  popupElement.classList.add('popup_opened'); // Добавляем класс, открывающий попап
+  document.addEventListener('keydown', closeOnEsc); // Добавляем слушатель закрытия по ESC
 }
