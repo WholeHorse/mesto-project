e62e5ce5d8de968137ec970453a6514c09136b1e
Mikhail Burykin <whitecrazygoose@mail.ru>
2024-12-06 15:32:56 +0300

    card refactoring & minor changes

--- src/components/card.js
+++ src/components/card.js
@@ -1,74 +1,105 @@
 import { clickLargeImage } from "./index.js";
-import { getCards, deleteCard, likeCard, deleteLikeCard } from './api.js'
-export { createCard, clickLikeCard, renderCard, updateLikeCounter }
+import { deleteCard, likeCard, deleteLikeCard } from './api.js';
+export { createCard, clickLikeCard, renderCard, updateLikeCounter };
 
-// Добавление новых карточек
+// Контейнер для карточек
 const cardsContainer = document.querySelector('.cards-grid');
-const userTemplate = document.querySelector('#card-template').content; // выбираем темплейт
 
-// создание новых карточек
+// Шаблон карточки
+const userTemplate = document.querySelector('#card-template').content;
+
+/**
+ * Создает карточку из переданной информации.
+ * @param {Object} cardsInfo - Информация о карточке.
+ * @param {Object} userInfo - Информация о текущем пользователе.
+ * @returns {HTMLElement} - DOM-элемент карточки.
+ */
 function createCard(cardsInfo, userInfo) {
-  const cardElement = userTemplate.querySelector('.place').cloneNode(true); //клонируем див внутри него со всем содержимым
-  const cardElementPhoto = cardElement.querySelector('.place__photo'); // картинка как элемент карточки
+  // Клонируем шаблон карточки
+  const cardElement = userTemplate.querySelector('.place').cloneNode(true);
+  const cardElementPhoto = cardElement.querySelector('.place__photo');
   const btnLike = cardElement.querySelector('.place__like-button');
-  const counterLikes = cardElement.querySelector('.place__like-counter')
+  const counterLikes = cardElement.querySelector('.place__like-counter');
 
-  // Кнопка удаления только под своими карточками
+  // Настройка кнопки удаления, доступной только для своих карточек
   if (userInfo._id === cardsInfo.owner._id) {
-    cardElement.querySelector('.place__delete-button').addEventListener('click', (event) => clickDeleteCard(event, cardsInfo._id)) // слушатель на кнопку удаления карточки
+    cardElement.querySelector('.place__delete-button').addEventListener('click', (event) => {
+      clickDeleteCard(event, cardsInfo._id);
+    });
   } else {
-    const btnDelete = cardElement.querySelector('.place__delete-button')
-    btnDelete.remove()
+    cardElement.querySelector('.place__delete-button').remove();
   }
 
-  cardElement.querySelector('.place__like-counter').textContent = cardsInfo.likes.length;
-  cardElement.querySelector('.place__name').textContent = cardsInfo.name; //подставляем название места из инпута
-  cardElementPhoto.src = cardsInfo.link; // подставляем ссылку из инпута
-  cardElementPhoto.alt = cardsInfo.name // альтернативный текст если на загрузится
-  cardElementPhoto.addEventListener('click', function () { clickLargeImage(cardsInfo.link, cardsInfo.name) }) // слушатель на картинку для открытия попапа - передаем ему 2 аргумента
+  // Заполнение данных карточки
+  counterLikes.textContent = cardsInfo.likes.length;
+  cardElement.querySelector('.place__name').textContent = cardsInfo.name;
+  cardElementPhoto.src = cardsInfo.link;
+  cardElementPhoto.alt = cardsInfo.name;
 
-  btnLike.addEventListener('click', (event) => clickLikeCard(event, cardsInfo._id, counterLikes)) // Слушатель на кнопку лайк
+  // Слушатель для увеличения изображения
+  cardElementPhoto.addEventListener('click', () => {
+    clickLargeImage(cardsInfo.link, cardsInfo.name);
+  });
 
-  // Отрисуем сердечки если уже лайкали раньше
-  cardsInfo.likes.forEach(element => {
-    if (element._id === userInfo._id) {
-      btnLike.classList.add('place__like-button_active')
-    }
-  })
-  return cardElement
+  // Слушатель для кнопки лайка
+  btnLike.addEventListener('click', (event) => {
+    clickLikeCard(event, cardsInfo._id, counterLikes);
+  });
+
+  // Установка активного состояния лайка, если текущий пользователь уже лайкал
+  if (cardsInfo.likes.some((like) => like._id === userInfo._id)) {
+    btnLike.classList.add('place__like-button_active');
+  }
+
+  return cardElement;
 }
 
-// отображение новых карточек в DOM
+/**
+ * Отображает карточку в DOM, добавляя её в начало контейнера.
+ * @param {HTMLElement} card - DOM-элемент карточки.
+ */
 function renderCard(card) {
-  cardsContainer.prepend(card); // вставляем наш контейнер с названием и ссылкой в начало блока карточек
+  cardsContainer.prepend(card);
 }
 
-// Функция коллбэк для слушателя на кнопке удалить.
+/**
+ * Обработчик удаления карточки.
+ * @param {Event} event - Событие клика.
+ * @param {string} cardId - ID карточки для удаления.
+ */
 function clickDeleteCard(event, cardId) {
   deleteCard(cardId)
-    .then(() => { event.target.closest('.place').remove() })
-    .catch((res) => console.log(res))
+      .then(() => {
+        event.target.closest('.place').remove();
+      })
+      .catch((err) => console.error(`Ошибка при удалении карточки: ${err}`));
 }
 
-// Кнопка лайк.
+/**
+ * Обработчик кнопки лайка.
+ * @param {Event} event - Событие клика.
+ * @param {string} cardId - ID карточки для лайка.
+ * @param {HTMLElement} counterLikes - Элемент для отображения количества лайков.
+ */
 function clickLikeCard(event, cardId, counterLikes) {
-  if (event.target.classList.contains('place__like-button_active')) {
-    deleteLikeCard(cardId)
+  const btnLike = event.target;
+  const likePromise = btnLike.classList.contains('place__like-button_active')
+      ? deleteLikeCard(cardId)
+      : likeCard(cardId);
+
+  likePromise
       .then((res) => {
-        updateLikeCounter(res, counterLikes)
-        event.target.classList.remove('place__like-button_active')
+        updateLikeCounter(res, counterLikes);
+        btnLike.classList.toggle('place__like-button_active');
       })
-      .catch((res) => console.log(res))
-  } else {
-    likeCard(cardId)
-      .then((res) => {
-        updateLikeCounter(res, counterLikes)
-        event.target.classList.add('place__like-button_active')
-      })
-      .catch((res) => console.log(res))
-  }
+      .catch((err) => console.error(`Ошибка при обработке лайка: ${err}`));
 }
 
+/**
+ * Обновляет количество лайков на карточке.
+ * @param {Object} res - Ответ сервера.
+ * @param {HTMLElement} counterLikes - Элемент для отображения количества лайков.
+ */
 function updateLikeCounter(res, counterLikes) {
-  counterLikes.textContent = res.likes.length
+  counterLikes.textContent = res.likes.length;
 }
